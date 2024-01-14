f3bf29150a0b106b5e408659241162b9825bbd2b
MohanedB <90917177+MohanedB@users.noreply.github.com>
2024-01-14 14:13:50 -0500

    Power Ups
    Made a spawner that spawns a power ups every 10 seconds and one spawns as soon as the game starts. Then When you take the power up, you either get a boost of 1 damage or 2

---
+++ Assets/Code/PowerUpsCreation.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpsCreation : MonoBehaviour
+{
+    [SerializeField]
+    GameObject c;
+    private Camera _mainCam;
+    private int offset = 100;
+    private float _screenWidth;
+    private float _screenHeight;
+
+    void Start()
+    {
+        _mainCam = Camera.main;
+
+        _screenWidth = _mainCam.orthographicSize * _mainCam.aspect;
+        _screenHeight = _mainCam.orthographicSize;
+
+        InvokeRepeating("SpawnPowerup", 0, 9);
+        Debug.Log("A power up has been spawned");
+    }
+
+    private Vector3 GetRandomSpawnerPosition()
+    {
+
+        float randomX = Random.Range(0f, 1f);
+
+
+        float fixedY = 0.9f;
+
+
+        Vector3 newPos = _mainCam.ViewportToWorldPoint(new Vector3(randomX, fixedY, 0f));
+        newPos.z = 0;
+        return newPos;
+    }
+
+    private void SpawnPowerup()
+    {
+        transform.position = GetRandomSpawnerPosition();
+        c.GetComponent<Rigidbody2D>().velocity = transform.up * -1;
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Destroy")
+        {
+            Destroy(c);
+            Debug.Log("It has been destroyed");
+        }
+    }
+
+
+}
